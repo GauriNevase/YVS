95e85e88e5a6fcd6a300d904da039da9914c2c1e
GauriNevase <110117024+GauriNevase@users.noreply.github.com>
2024-03-04 16:18:51 +0530

    minor changes

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
-burger=document.querySelector('.burger')
-navbar=document.querySelector('.navbar')
-navList=document.querySelector('.nav-list')
-rightNav=document.querySelector('.rightNav')
+// burger=document.querySelector('.burger')
+// navbar=document.querySelector('.navbar')
+// navList=document.querySelector('.nav-list')
+// rightNav=document.querySelector('.rightNav')
 
-burger.addEventListener('click',()=>{
-    rightNav.classList.toggle('v-class-resp');
-    navList.classList.toggle('v-class-resp');
-    navbar.classList.toggle('h-nav-resp');
-})
+// burger.addEventListener('click',()=>{
+//     rightNav.classList.toggle('v-class-resp');
+//     navList.classList.toggle('v-class-resp');
+//     navbar.classList.toggle('h-nav-resp');
+// })
+
+/* Demo purposes only */
+// $(".hover").mouseleave(
+//     function() {
+//       $(this).removeClass("hover");
+//     }
+//   );
+
+
+// our goals
+  function openTab(tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    document.getElementById(tabName).style.display = "block";
+
+    tabcontent.addEventLitener('onclick',()=>{
+        nav.nav-ink.toggle('nav-link.active');
+    })
+}
